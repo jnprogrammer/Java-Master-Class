b4381284a8bd8c2cd857c9cabdbd65434a3ed58f
jnprogrammer <joshuanparker@gmail.com>
2017-11-01 15:53:50 -0400

    Added exercise on bool and char

---
+++ char_bool/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char myChar = '\u00AE';
+	boolean myboolT = true;
+	boolean myboolF = false;
+	System.out.println("Unicode output was for the register symbol " + myChar);
+    }
+}
